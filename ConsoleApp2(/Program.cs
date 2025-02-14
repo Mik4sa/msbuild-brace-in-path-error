06c68d57272b68759824f42c510d3c97fef23613
Kai Leonhardt <8343141+Mik4sa@users.noreply.github.com>
2025-02-14 13:40:09 +0100

    Initial commit

---
+++ ConsoleApp2(/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var content = File.ReadAllText("test.txt");
+                Console.WriteLine("File content: " + content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
